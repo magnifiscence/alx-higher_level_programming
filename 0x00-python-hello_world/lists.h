d80876109591a1f7b12a39695c2fd147c9c6fd1a
magnifiscence <www.divinedivinepraise@gmail.com>
2023-08-09 19:11:54 -0700

    0-run

---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,22 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+/**
+ * struct listint_s - a single linked list.
+ * @n: integer.
+ * @next: pointer
+ * Description: singly linked list node structure.
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+int check_cycle(listint_t *list);
+void free_listint(listint_t **head, const int n);
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+
+#endif
