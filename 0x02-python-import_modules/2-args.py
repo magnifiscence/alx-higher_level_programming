9c674e245a559644ded1b231bd245a1e9fea0840
magnifiscence <www.divinedivinepraise@gmail.com>
2023-08-12 00:17:05 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,9 +6,9 @@ if __name__ == "__main__":
 
     count = len(sys.argv) - 1
     if count == 0:
-        print("0 arguments")
+        print("0 arguments.\n")
     elif count == 1:
-        print("1 argument:")
+        print("1 argument:\n")
     else:
         print("{} arguments:".format(count))
         for i in range(count):
