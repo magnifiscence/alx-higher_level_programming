2fb24b90ca0194c50d2de376d7c6a5983eff89cf
magnifiscence <www.divinedivinepraise@gmail.com>
2023-08-14 08:59:41 -0700

    1-element_at.py

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -4,11 +4,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-size_t print_listint(const listint_t *h);
-listint_t *add_nodeint_end(listint_t **head, const int n);
-void free_listint(listint_t *head);
-int is_palindrome(listint_t **head);
-
 /**
  * struct listint_s - singly linked list structure
  * @n: int
@@ -19,5 +14,10 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int is_palindrome(listint_t **head);
 
 #endif
