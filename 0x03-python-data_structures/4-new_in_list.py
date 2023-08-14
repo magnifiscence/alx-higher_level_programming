0ccf9046412c5c333a2f86a16b11fbd1eb3728f4
magnifiscence <www.divinedivinepraise@gmail.com>
2023-08-14 09:47:25 -0700

    4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+""" function replaces an element in a newly copied list in a specified position"""
+if idx <0 or idx> len(my_list -1):
+    return (my_list)
+
+copy = [x for x in my_list]
+copy[idx] = element
+return (copy)
