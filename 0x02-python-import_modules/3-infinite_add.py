e827fc2e58652a823d05fbb19b0fa0099d90b386
magnifiscence <www.divinedivinepraise@gmail.com>
2023-08-15 09:17:02 -0700

    python

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def add_arg(argv):
     n = len(argv) - 1
     if n == 0:
@@ -11,7 +12,6 @@ def add_arg(argv):
             add += int(argv[i])
             i += 1
             print("{:d}".format(add))
-
 if __name__ == "__main__":
     import sys
     add_arg(sys.argv)
