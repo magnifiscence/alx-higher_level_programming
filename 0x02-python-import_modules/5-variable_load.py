a97d015507bf313065c1ab2979af985a4ada660a
magnifiscence <www.divinedivinepraise@gmail.com>
2023-08-15 09:22:19 -0700

    5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -4,4 +4,8 @@ if __name__ =="__main__":
     """prints the value of a variable a"""
     fromm variable_load_5 import a
 
-    print(a)
+    def find_var():
+        print(a)
+
+        if __name__ == "__main__":
+            find_var()
