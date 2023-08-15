27fed405d36a9809e442effdae613c985f4b2b2c
magnifiscence <www.divinedivinepraise@gmail.com>
2023-08-15 09:57:54 -0700

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,18 +2,27 @@
 
 if __name__ == "__main__":
     """program for handling arithmetic operations"""
-    from calculator_1 import add, sub, mul, div
     import sys
 
-    if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a><operator><b>")
+    nargs = len(sys.argv) - 1
+    if nargs != 3:
+        print("usage: ./100-my_calculator.py <a>  <operator> <b>")
         sys.exit(1)
 
-        op = {"+": add, "-": sub, "*": mul, "/": div}
-        if sys.argv[2] not in list(op.keys()):
+        op = sys.arg[2]
+        if op != '+' and op != '-' and op != '*' and op != '/':
             print("unknown operator. Available operators: +, -, * and /")
             sys.exit(1)
 
-            a = int(sys.arg[1])
+            from calculator_1 import add, sub, mul, div
+            a = int(sys.argv[1])
             b = int(sys.argv[3])
-            print("{} {} {} = {}".format(a, sys.argv[2], b, op[sys.argv[2]](a, b)))
+
+            if op == '+':
+                print("{} + {} = {}".format(a, b, add(a, b)))
+            elif op == '-':
+                print("{} - {} = {}".format(a, b, sub(a, b)))
+            elif op == '*':
+                print("{} * {} = {}".format(a, b, mul(a, b)))
+            else:
+                print("{} / {} = {}".format(a, b, div(a, b)))
