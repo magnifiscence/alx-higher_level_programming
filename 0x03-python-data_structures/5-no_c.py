77ee1dd3c05282091e0233711bc90b9bcf501043
magnifiscence <www.divinedivinepraise@gmail.com>
2023-08-15 19:45:48 -0700

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = ''
-    for item in my_string:
-        if item == 'c' or item == 'C':
-            item = ''
-            new_string += item
-            return (new_string)
+    new_str = ""
+    for i in my_string:
+        if (i != 'c') and (i != 'C'):
+            new_str += i
+            return (new_str)
