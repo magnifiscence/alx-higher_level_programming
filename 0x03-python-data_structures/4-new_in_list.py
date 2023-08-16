480c98debd99473f9c44d9ee981387da3a66039c
magnifiscence <www.divinedivinepraise@gmail.com>
2023-08-15 19:27:46 -0700

    p

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
+def new_in_list(my_list, idx, element):
 """ function replaces an element in a newly copied list in a specified position"""
-if idx <0 or idx> len(my_list -1):
+if idx < 0 or idx > len(my_list) - 1:
     return (my_list)
 
 my_list2 = my_list.copy()
