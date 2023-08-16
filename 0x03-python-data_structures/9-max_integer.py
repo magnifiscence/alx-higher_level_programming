dec11eaaae5fb59be783022377f147f79e863eb7
magnifiscence <www.divinedivinepraise@gmail.com>
2023-08-15 20:21:52 -0700

    9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-def max_integer(my_list=[]):
+def max_integer(my_list = []):
     """function that finds the biggest integer of a list."""
     if len(my_list) == 0:
         return (None)
 
     biggest = my_list[0]
-    for j in range(len(my_list)):
+    for j in range(1, length):
         if my_list[j] > biggest:
             biggest = my_list[j]
 
