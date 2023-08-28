4e31d4f77410e5fdd810656523b450d041331206
magnifiscence <www.divinedivinepraise@gmail.com>
2023-08-28 10:53:41 -0700

    task done

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,9 +6,9 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end='')
         except:
-            break:
-                else:
-                    count += 1
+            break
+        else:
+            count += 1
 
-                    print()
-                    return (count)
+    print()
+    return (count)
