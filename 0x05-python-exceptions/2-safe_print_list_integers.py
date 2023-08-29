8c2ceb0177377b2cec631a14d39e809d22e81812
magnifiscence <www.divinedivinepraise@gmail.com>
2023-08-28 18:16:55 -0700

    task done

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    import sys
+    try:
+        print("{:d}.format(value))
+        except Exception as i:
+        sys.stderr.write("Exception: {}\n".format(i))
+        return (False)
+        else:
+        return (True)
