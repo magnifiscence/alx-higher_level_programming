94e37003c8ece5ba0091c9d2c60b972201ea6452
magnifiscence <www.divinedivinepraise@gmail.com>
2023-08-30 06:45:50 -0700

    task done

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -9,13 +9,13 @@ class Square:
     class Square definition
 
     Args:
-    size: size of side of a square
+        size: size of side of a square
     """
     def __init__(self, size):
         """
         initializes square
 
         Attributes:
-        size: size of side of a square
+            size: size of side of a square
         """
         self.__size = size
