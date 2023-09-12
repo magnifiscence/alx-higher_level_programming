88ba30b6dc615b2006ed2bdb1ec8dd5bd9972dfa
magnifiscence <www.divinedivinepraise@gmail.com>
2023-09-12 11:04:51 -0700

    task done

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,21 +1,10 @@
 #!/usr/bin/python3
-"""
-Module 2-read_lines
-
-Contains function that reads n lines and prints to stdout
-"""
+"""Defines a function that appends a string to a file"""
 
 
-def read_lines(filename="", nb_lines=0):
-    """reads n lines and prints to stdout
-    Print:
-        n lines: if n
-        entire file: if n is less than 1 or greater than lines in file
-    """
-    with open(filename, mode="r", encoding="utf-8") as f:
-        if nb_lines <= 0:
-            print(f.read(), end="")
-        else:
-            while nb_lines:
-                print(f.readline(), end="")
-                nb_lines -= 1
+def append_write(filename="", text=""):
+    """Appends a string at the end of a text file (UTF8)
+    and returns the number of characters added"""
+    with open(filename, 'a', encoding="utf-8") as f:
+        ret = f.write(text)
+    return ret
