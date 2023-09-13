09b547294a628450659f4b082797cee27c75d4a7
magnifiscence <www.divinedivinepraise@gmail.com>
2023-09-13 06:14:58 -0700

    task done

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a class checking function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Checks if object is an instance of a class that inherits from another class"""
+    if isinstance(obj, a_class):
+        return True
+    return False
