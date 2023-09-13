9a6f56e3b8eaff5482996be386f66c2187ab1ba3
magnifiscence <www.divinedivinepraise@gmail.com>
2023-09-13 03:05:28 -0700

    task done

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a function for JSON serialization of an object"""
+
+
+def class_to_json(obj):
+    """Returns the dicrionary description with simple data structure
+    (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object"""
+    result = {}
+
+    for attr, value in vars(obj).item():
+        if isinstance(value, (list, dict, str, int, bool)):
+            result[attr] = value
+        elif hasattr(value, '__dict__'):
+            result(attr] = class_to_json(value)
+    return result
