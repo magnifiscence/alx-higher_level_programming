3216365b1ba83dcfca377d8bd381eaa52e3532b4
magnifiscence <www.divinedivinepraise@gmail.com>
2023-09-13 07:06:08 -0700

    task done

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a class that inherits from BaseGeometry"""
+
+
+BaseGometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Instantiation with width and height also validated
+    by integer validator"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.height = height
