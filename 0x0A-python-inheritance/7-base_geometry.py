f46caf5ff64d02a6ef8d8bf2970ee7aade7e7082
magnifiscence <www.divinedivinepraise@gmail.com>
2023-09-13 06:49:39 -0700

    task done

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/python3
+"""Defines a class"""
+
+
+class BaseGeometry():
+    """Defines a class"""
+    def area(self):
+        """Raise an exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates the value"""
+        self.value = value
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
