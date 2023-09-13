4f83483cd1c86138356556734efa8344a1db6227
magnifiscence <www.divinedivinepraise@gmail.com>
2023-09-13 02:54:07 -0700

    task done

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""Adds all arguments to a Python list and saves them to a file"""
+"""Adds all arguments to a Python list, and then saves them to a file"""
 import sys
 
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-arguments = sys,argv[1:]
+arguments = sys.argv[1:]
 
 # Load the existing data from the JSON file if it exists
 try:
@@ -14,7 +14,7 @@ try:
 except FileNotFoundError:
     data = []
 
-# Add the new argument to the data list
+# Add the new arguments to the data list
 data.extend(arguments)
 
 # Save the updated data to the JSON file
