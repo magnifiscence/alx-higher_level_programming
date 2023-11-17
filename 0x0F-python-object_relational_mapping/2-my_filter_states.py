7d901fc7b3800ff0c413fd5ad1ce8f5ba41f923e
magnifiscence <www.divinedivinepraise@gmail.com>
2023-11-17 09:14:32 -0800

    task done

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,8 +7,8 @@ from sys import argv
 
 if __name__ == "__main__":
     """ Lists all states from the database hbtn_0e_0_usa """
-    my_db = MySQLdb.connect(host="localhost", port=3306,
-                            user=argv[1], password=argv[2], db=argv[3])
+    my_db = MySQLdb.connect(host="localhost", port=3306, mysql username=argv[1],
+                            mysql password=argv[2], database name=argv[3])
     cur = my_db.cursor()
     cur.execute("""SELECT * FROM states WHERE name LIKE BINARY '{}' \
     ORDER BY id ASC""".format(argv[4]))
