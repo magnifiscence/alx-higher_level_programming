ebb23332eefc9fca59a5e373a28ca897a2d99c5a
magnifiscence <www.divinedivinepraise@gmail.com>
2023-11-18 19:26:13 -0800

    task done

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,12 +6,12 @@ from sys import argv
 
 if __name__ == "__main__":
     """ Lists all states from the database hbtn_0e_0_usa """
-    my_db = MySQLdb.connect(host="localhost", port=3306,
-                            user=argv[1], password=argv[2], my_db=argv[3])
-    cur = my_db.cursor()
+    db = MySQLdb.connect(host="localhost", port=3306,
+                            user=argv[1], password=argv[2], db=argv[3])
+    cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cur.fetchall()
     for ret in rows:
         print(ret)
     cur.close()
-    my_db.close()
+    db.close()
